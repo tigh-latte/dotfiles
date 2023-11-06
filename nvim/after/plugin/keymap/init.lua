e21417da8526e7706f411a1841955fba0941f355
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2023-11-06 21:37:38 +0000

    even more

--- nvim/after/plugin/keymap/init.lua
+++ nvim/after/plugin/keymap/init.lua
@@ -1,2 +1,12 @@
---vim.keymap.set("n", "`", "<Nop>", { silent = true, remap = false })
-vim.g.mapleader = "\\"
+local fn = ({
+	['Darwin'] = function()
+		vim.keymap.set("n", "`", "<Nop>", { silent = true, remap = false })
+		vim.g.mapleader = "`"
+	end,
+})[vim.loop.os_uname().sysname]
+
+if not fn then
+	fn = function() vim.g.mapleader = "\\" end
+end
+
+fn()
--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -2,5 +2,6 @@ require("mason").setup()
 require("mason-lspconfig").setup {
 	ensure_installed = {
 		"gopls",
+		"jedi_language_server",
 	}
 }
