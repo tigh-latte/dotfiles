c2af8862bd35f2aa937282b99c25ceee8554a0d9
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2023-11-07 09:33:18 +0000

    cleaner install

--- install.sh
+++ install.sh
@@ -14,6 +14,21 @@ main() {
 	## install home level content
 	for file in .*; do
 		[[ " ${__IGNORE_FILES[*]} " =~ " ${file} " ]] && continue
+
+		local config_dest=${HOME}/${file}
+
+		# Config file exists and is a symlink
+		([ -f "$config_dest" ] && [ -L "$config_dest" ]) && {
+			echo "skipping, already installed: ${config_dest}"
+			continue
+		}
+		# Config file exists but not as a symlink (hasn't been installed via this script)
+		([ -f "$config_dest" ] && [ ! -L "$config_dest" ]) && {
+			echo "${config_dest} exists on machine. Performing backup and removal."
+			mv ${config_dest} ${config_dest}.tigh-latte.bkp
+		}
+
+		# install config.
 		ln -s ${PWD}/${file} ${HOME}/${file}
 	done
 
@@ -21,6 +36,20 @@ main() {
 	for file in *; do
 		[ ! -d "${file}" ] && continue
 
+		local config_dest=${HOME}/.config/${file}
+
+		# Config dir exists and is a symlink
+		([ -d "$config_dest" ] && [ -L "$config_dest" ]) && {
+			echo "skipping, already installed: ${config_dest}"
+			continue
+		}
+		# Config dir exists but not as a symlink (hasn't been installed via this script)
+		([ -d "$config_dest" ] && [ ! -L "$config_dest" ]) && {
+			echo "${config_dest} exists on machine. Performing backup and removal."
+			mv ${config_dest} ${config_dest}.tigh-latte.bkp
+		}
+
+		# install config.
 		ln -s ${PWD}/${file} ${HOME}/.config/${file}
 	done
 }
