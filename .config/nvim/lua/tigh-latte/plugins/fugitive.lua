0fa199794e70328948462339e936712f014612f0
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-03-12 21:54:56 +0000

    use fugitive :Git for status instead of

--- .config/nvim/lua/tigh-latte/plugins/fugitive.lua
+++ .config/nvim/lua/tigh-latte/plugins/fugitive.lua
@@ -1,14 +1,13 @@
 return {
 	"tpope/vim-fugitive",
 	config = function()
-		vim.keymap.set("n", "<Leader>git", vim.cmd.Git)
+		local opts = { noremap = true, silent = true }
+		vim.keymap.set("n", "<Leader>git", vim.cmd.Git, opts)
 
-		vim.keymap.set("n", "<Leader>gst", function()
-			vim.cmd.Git("status")
-		end, { noremap = true, silent = true })
+		vim.keymap.set("n", "<Leader>gst", vim.cmd.Git, opts)
 
 		vim.keymap.set("n", "<Leader>gbl", function()
 			vim.cmd.Git("blame")
-		end, { noremap = true, silent = true })
+		end, opts)
 	end,
 }
