57d7b98531a7fea158cfa4bacf4dd50d499b764d
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-03-12 21:56:23 +0000

    use better (sane) diagnotics symbols

--- .config/nvim/lua/tigh-latte/lsp/init.lua
+++ .config/nvim/lua/tigh-latte/lsp/init.lua
@@ -6,7 +6,7 @@ M.setup = function()
 	require("tigh-latte.lsp.cmp").setup()
 	require("tigh-latte.lsp.lang")
 
-	local signs = { Error = "•", Warn = "•", Hint = "", Info = "" }
+	local signs = { Error = "•", Warn = "•", Hint = "•", Info = "•" }
 	for type, icon in pairs(signs) do
 		local hl = "DiagnosticSign" .. type
 		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
