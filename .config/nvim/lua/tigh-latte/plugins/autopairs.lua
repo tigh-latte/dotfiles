002ec5cc2f49cb836f6a825d8923bf4e945f10ae
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-04-10 16:00:01 +0100

    auto insert brackets with autopairs on complete

--- .config/nvim/lua/tigh-latte/plugins/autopairs.lua
+++ .config/nvim/lua/tigh-latte/plugins/autopairs.lua
@@ -1,5 +1,14 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		require("nvim-autopairs").setup()
+		local cmp_ap = require("nvim-autopairs.completion.cmp")
+		local cmp = require("cmp")
+		cmp.event:on("confirm_done", cmp_ap.on_confirm_done())
+	end,
 	opts = {},
 }
