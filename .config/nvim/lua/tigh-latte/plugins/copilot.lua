b29f6d87e7af48a6972d1151a304ed216c4993d6
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-04-17 22:43:26 +0100

    disable copilot by default

--- .config/nvim/lua/tigh-latte/plugins/copilot.lua
+++ .config/nvim/lua/tigh-latte/plugins/copilot.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("copilot").setup({
 			suggestion = {
-				enabled = true,
+				enabled = false,
 				auto_trigger = true,
 				keymap = {
 					next = "<M-n>",
