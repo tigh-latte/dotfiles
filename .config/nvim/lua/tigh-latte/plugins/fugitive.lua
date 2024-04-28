a5c4d5720ce6f093d6a83af48dac699d3d40ad7d
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-04-28 22:07:49 +0100

    add vim rhubarb to fugitive

--- .config/nvim/lua/tigh-latte/plugins/fugitive.lua
+++ .config/nvim/lua/tigh-latte/plugins/fugitive.lua
@@ -1,5 +1,8 @@
 return {
 	"tpope/vim-fugitive",
+	dependencies = {
+		"tpope/vim-rhubarb",
+	},
 	config = function()
 		local opts = { noremap = true, silent = true }
 		vim.keymap.set("n", "<Leader>git", vim.cmd.Git, opts)
@@ -11,5 +14,7 @@ return {
 		vim.keymap.set("n", "<Leader>glog", function()
 			vim.cmd.Git("log -p %")
 		end)
+
+		vim.keymap.set("n", "<Leader>ghub", vim.cmd.GBrowse, opts)
 	end,
 }
