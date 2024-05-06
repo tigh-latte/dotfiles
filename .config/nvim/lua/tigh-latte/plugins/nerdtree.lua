c07ecd39c8429b08fc3c39ca3e7459f057021600
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-05-06 17:20:48 +0100

    get rid of unused nerdtree config

--- .config/nvim/lua/tigh-latte/plugins/nerdtree.lua
+++ .config/nvim/lua/tigh-latte/plugins/nerdtree.lua
@@ -7,14 +7,7 @@ return {
 
 	config = function()
 		vim.g.NERDTreeIgnore = { "\\.pyc$", "\\~$", "\\.swp$" }
-		vim.g.NERDTreeGitStatusShowIgnored = 1
-		vim.g.NERDTreeGitStatusConcealBrackets = 1
 		vim.keymap.set("n", "<Leader>n", vim.cmd.NERDTreeToggle, { noremap = true, silent = true })
 		vim.keymap.set("n", "<Leader>f", vim.cmd.NERDTreeFind, { noremap = true, silent = true })
-		vim.cmd([[
-    let g:NERDTreeGitStatusIndicatorMapCustom = {
-      \ 'Ignored'     : '!',
-      \ }
-]])
 	end,
 }
