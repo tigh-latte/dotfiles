019ef3e7cb2e8a3cbe21e21f092473c9f50f231d
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-05-14 12:23:41 +0100

    explicitly set dark theme now that we cannot override with env

--- .config/nvim/lua/tigh-latte/plugins/theme.lua
+++ .config/nvim/lua/tigh-latte/plugins/theme.lua
@@ -5,7 +5,7 @@ return {
 	event = "VeryLazy",
 	config = function()
 		require("onedark").setup({
-			style = "light",
+			style = "dark",
 			term_colors = false,
 			transparent = true,
 			toggle_style_key = "<Leader>ts",
