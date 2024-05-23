5a17d7aec1664b92d5a5c4da1460fb0a42c16449
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-05-23 23:04:28 +0100

    nicer os map with default, just incase I ever use the cursed OS again, or somehow end up on like BSD

--- .config/nvim/lua/tigh-latte/init.lua
+++ .config/nvim/lua/tigh-latte/init.lua
@@ -1,18 +1,14 @@
 -- Custom leader key per OS. This was relevant when I used the mac keyboard, but isn't
 -- now. Am keeping just incase OS specific stuff is needed in the future.
-local fn = ({
-	["Darwin"] = function()
+(setmetatable({
+	Darwin = function()
 		vim.keymap.set("n", "`", "<Nop>", { silent = true, remap = false })
 		vim.g.mapleader = "`"
 	end,
-})[vim.loop.os_uname().sysname]
-
-if not fn then
-	fn = function()
-		vim.g.mapleader = " "
-	end
-end
-
-fn()
+}, {
+	__index = function(_, _)
+		return function() vim.g.mapleader = " " end
+	end,
+}))[vim.loop.os_uname().sysname]()
 
 require("tigh-latte.lazy")
