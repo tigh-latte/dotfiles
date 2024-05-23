1386fe1697f9a0b25845d4319b4cc5e7391fc3b1
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-05-23 14:28:04 +0100

    more telescope ignores

--- .config/nvim/lua/tigh-latte/plugins/telescope.lua
+++ .config/nvim/lua/tigh-latte/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
 	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 	},
@@ -13,6 +14,8 @@ return {
 					"dist",
 					"build/",
 					"*.min.*",
+					"__.*",
+					"tmp/*",
 					".git/",
 					".venv/",
 				},
