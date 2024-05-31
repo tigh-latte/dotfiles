791b2362a0f98a179e9e97336f7f15bc7c2c4938
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-05-31 22:01:09 +0100

    zmk editor plugin

---
+++ .config/nvim/lua/tigh-latte/plugins/zmk.lua
@@ -0,0 +1,38 @@
+return {
+	"codethread/qmk.nvim",
+	ft = "dts",
+	config = function()
+		require("qmk").setup({
+			name = "glove80",
+			variant = "zmk",
+			layout = {
+				"x x x x x _ _ _ _ _ _ _ _ _ x x x x x",
+				"x x x x x x _ _ _ _ _ _ _ x x x x x x",
+				"x x x x x x _ _ _ _ _ _ _ x x x x x x",
+				"x x x x x x _ _ _ _ _ _ _ x x x x x x",
+				"x x x x x x x x x _ x x x x x x x x x",
+				"x x x x x _ x x x _ x x x _ x x x x x",
+			},
+			comment_preview = {
+				keymap_overrides = {
+					["iso\\"] = "\\",
+					["LS%(N1%)"] = "!",
+					["LS%(N2%)"] = '"',
+					["LS%(N3%)"] = "£",
+					["LS%(N4%)"] = "$",
+					["LS%(N5%)"] = "%%",
+					["LS%(N6%)"] = "^",
+					["LS%(N7%)"] = "&",
+					["LS%(N8%)"] = "*",
+					["LS%(MINUS%)"] = "_",
+					["LS%(SEMI%)"] = ":",
+					["LS%(EQUAL%)"] = "+",
+					["LS%(NON_US_BSLH%)"] = "|",
+					["LS%(BSLH%)"] = "~",
+					["RA%(N4%)"] = "€",
+					["&magic MAGIC 0"] = "✨",
+				},
+			},
+		})
+	end,
+}
