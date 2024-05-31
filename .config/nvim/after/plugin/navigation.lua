2d84ece420d9addbad909c0115b629e7242a73dd
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-05-31 21:36:20 +0100

    have navigation use better nvim api, also allow panel hopping in visual mode

--- .config/nvim/after/plugin/navigation.lua
+++ .config/nvim/after/plugin/navigation.lua
@@ -1,12 +1,12 @@
 -- Simple split window navigation.
-vim.api.nvim_set_keymap("n", "<C-H>", "<C-W><C-H>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-J>", "<C-W><C-J>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-K>", "<C-W><C-K>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-L>", "<C-W><C-L>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-H>", "<C-W><C-H>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-J>", "<C-W><C-J>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-K>", "<C-W><C-K>", { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-L>", "<C-W><C-L>", { noremap = true, silent = true })
 
 -- Use , and . to change the indection within the current set of { }
-vim.api.nvim_set_keymap("n", "<Leader>,", "<i}", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Leader>.", ">i}", { noremap = true })
+vim.keymap.set({ "n", "v" }, "<Leader>,", "<i}", { noremap = true })
+vim.keymap.set({ "n", "v" }, "<Leader>.", ">i}", { noremap = true })
 
 -- After paging the window forward or backward, recentre the screen on the cursor.
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
