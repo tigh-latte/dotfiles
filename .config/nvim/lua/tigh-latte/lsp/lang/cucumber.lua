a72404b832d07f851a39d5368f4a5f1c7b69a331
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-06-15 15:34:49 +0100

    remove cucumber lsp override

--- .config/nvim/lua/tigh-latte/lsp/lang/cucumber.lua
+++ .config/nvim/lua/tigh-latte/lsp/lang/cucumber.lua
@@ -1,5 +1,4 @@
 require("lspconfig").cucumber_language_server.setup({
-	cmd = { "/usr/bin/cucumber-language-server", "--stdio" },
 	on_attach = require("tigh-latte.lsp").make_on_attach(),
 	capabilities = vim.lsp.protocol.make_client_capabilities(),
 	settings = {
