40393e2ab6d7989fe2fc8996e0d6c67ff88a4db8
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-07-28 22:10:09 +0100

    another python treesitter injection for sql statements in django migrations

--- .config/nvim/queries/python/injections.scm
+++ .config/nvim/queries/python/injections.scm
@@ -7,3 +7,15 @@
     (string
       (string_content) @injection.content (#set! injection.language "sql"))
   ))
+
+(call
+  function: (attribute
+    object: (_) @_import (#eq? @_import "migrations")
+    attribute: (_) @_func (#eq? @_func "RunSQL"))
+  arguments: (argument_list
+    (keyword_argument
+      value: (list
+        (parenthesized_expression
+          (string
+            (string_content) @injection.content (#set! injection.language "sql")
+  ))))))
