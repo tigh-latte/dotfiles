8a471494779fe27dbb6ea0f5f2f7c662251cc0b0
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-08-23 21:26:15 +0100

    call lua function directly for trouble

--- .config/nvim/lua/tigh-latte/plugins/trouble.lua
+++ .config/nvim/lua/tigh-latte/plugins/trouble.lua
@@ -3,6 +3,11 @@ return {
 	config = function()
 		require("trouble").setup({})
 
-		vim.keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", { silent = true })
+		--vim.keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>", { silent = true })
+		vim.keymap.set("n", "<leader>tt", function()
+			require("trouble.api").toggle({
+				mode = "diagnostics",
+			})
+		end, { silent = true })
 	end,
 }
