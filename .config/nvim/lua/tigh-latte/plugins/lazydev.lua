dd05951e59a77134ba81f4ed1786c663c2b71e64
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-09-14 18:40:07 +0100

    switch to use my own fork of wezterm-types

--- .config/nvim/lua/tigh-latte/plugins/lazydev.lua
+++ .config/nvim/lua/tigh-latte/plugins/lazydev.lua
@@ -1,16 +1,17 @@
 return {
+	{ "Bilal2453/luvit-meta",     lazy = true },
+	{ "tigh-latte/wezterm-types", lazy = true },
 	{
 		"folke/lazydev.nvim",
+		ft = "lua",
 		config = function()
 			require("lazydev").setup({
+				---@type lazydev.Library.spec
 				library = {
-					{ path = "",              words = { "vim%.uv" } },
-					{ path = "wezterm-types", mods = { "wezterm" } },
+					{ path = "luvit-meta/library", words = { "vim%.uv" } },
+					{ path = "wezterm-types",      mods = { "wezterm" } },
 				},
-
 			})
 		end,
 	},
-	{ "Bilal2453/luvit-meta",        lazy = true },
-	{ "justinsgithub/wezterm-types", lazy = true },
 }
