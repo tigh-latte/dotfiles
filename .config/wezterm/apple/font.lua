dc8ff744de33a29d6d51ee590d8057b0b68780e8
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-09-19 00:47:40 +0100

    macos specific settings

---
+++ .config/wezterm/apple/font.lua
@@ -0,0 +1,8 @@
+---@module "wezterm"
+
+return {
+	---@param config Config
+	setup = function(config)
+		config.font_size = 11.0
+	end,
+}
---
+++ .config/wezterm/apple/ui.lua
@@ -0,0 +1,8 @@
+---@module 'wezterm'
+
+return {
+	---@param config Config
+	setup = function(config)
+		config.window_decorations = "RESIZE"
+	end,
+}
