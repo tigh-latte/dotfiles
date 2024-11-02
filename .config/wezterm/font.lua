11e53102c333f52d15c63c8f5ba6361267e54f0a
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-11-02 14:13:37 +0000

    add font size for laptop

--- .config/wezterm/font.lua
+++ .config/wezterm/font.lua
@@ -1,9 +1,11 @@
 return {
 	---@param config Config
 	setup = function(config)
-		---@module 'wezterm'
-
 		config.font_size = 9.0
+		if require("wezterm") --[[@as Wezterm]]:hostname() == "laptop" then
+			config.font_size = config.font_size
+		end
+
 		config.bold_brightens_ansi_colors = "No"
 		config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 	end,
