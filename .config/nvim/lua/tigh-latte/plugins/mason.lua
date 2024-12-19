7d9d02486ae3dd4a2e77287c2d09cd7b9d7a9423
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2024-12-19 22:41:35 +0000

    move all lsp configs to the mason plugin file. enable snippets on all languages

--- .config/nvim/lua/tigh-latte/plugins/mason.lua
+++ .config/nvim/lua/tigh-latte/plugins/mason.lua
@@ -1,7 +1,3 @@
-local function i(lang)
-	return function() require("tigh-latte.lsp.lang." .. lang) end
-end
-
 return {
 	{
 		"williamboman/mason.nvim",
@@ -26,12 +22,11 @@ return {
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
+			local lsps = require("lspconfig")
 			local capabilities = vim.tbl_extend("force",
 				{},
 				vim.lsp.protocol.make_client_capabilities(),
-				require("cmp_nvim_lsp").default_capabilities({
-					snippetSupport = false,
-				})
+				require("cmp_nvim_lsp").default_capabilities()
 			)
 
 			require("mason-lspconfig").setup({
@@ -46,19 +41,170 @@ return {
 					"yamlls",
 					"cucumber_language_server",
 				},
+
 				handlers = {
 					function(ls)
-						require("lspconfig")[ls].setup {
+						lsps[ls].setup({
 							on_attach = require("tigh-latte.lsp").make_on_attach(),
 							capabilities = capabilities,
-						}
+						})
 					end,
-					gopls = i("gopls"),
-					ts_ls = i("typescript"),
-					pyright = i("python"),
-					lua_ls = i("lua"),
-					taplo = i("toml"),
-					cucumber_language_server = i("cucumber"),
+
+					gopls = function()
+						lsps.gopls.setup({
+							on_attach = require("tigh-latte.lsp").make_on_attach({
+								on_save_actions = { "source.organizeImports" },
+							}),
+							capabilities = capabilities,
+							filetypes = { "go", "gomod", "gowork", "gotmpl" },
+							root_dir = require("lspconfig/util").root_pattern("go.work", "go.mod", ".git"),
+							single_file_support = true,
+							settings = {
+								gopls = {
+									buildFlags = { "-tags=integration" },
+									completeUnimported = true,
+									usePlaceholders = false,
+									vulncheck = "imports",
+									gofumpt = true,
+									staticcheck = true,
+									symbolScope = "workspace",
+									analyses = {
+										unusedparams = true,
+										nilness = true,
+										unusedwrite = true,
+										stdversion = true,
+										shadow = true,
+									},
+									hints = {
+										assignVariableTypes = true,
+										compositeLiteralFields = true,
+										compositeLiteralTypes = false,
+										constantValues = true,
+										functionTypeParameters = true,
+										parameterNames = true,
+										rangeVariableTypes = true,
+									},
+								},
+							},
+						})
+					end,
+
+					ts_ls = function()
+						lsps.ts_ls.setup({
+							on_attach = require("tigh-latte.lsp").make_on_attach({
+								on_save_actions = {
+									"source.sortImports.ts",
+									"source.addMissingImports.ts",
+									"source.removeUnusedImports.ts",
+								},
+							}),
+							capabilities = capabilities,
+							filetypes = {
+								"javascript", "javascriptreact", "javascript.jsx",
+								"typescript", "typescriptreact", "typescript.tsx",
+							},
+							root_dir = require("lspconfig/util").root_pattern(".git"),
+							single_file_support = true,
+							settings = {
+								typescript = {
+									format = {
+										indentSize = 2,
+										tabSize = 2,
+										convertTabsToSpaces = true,
+										semicolons = "remove",
+									},
+								},
+								javascript = {
+									format = {
+										indentSize = 2,
+										tabSize = 2,
+										convertTabsToSpaces = true,
+										semicolons = "remove",
+									},
+								},
+							},
+						})
+					end,
+
+					pyright = function()
+						lsps.pyright.setup({
+							on_attach = require("tigh-latte.lsp").make_on_attach(),
+							capabilities = capabilities,
+							filetypes = { "python" },
+							root_dir = require("lspconfig/util").root_pattern(".git"),
+							single_file_support = true,
+							settings = {
+								python = {
+									useLibraryCodeForTypes = true,
+									analysis = {
+										autoSearchPaths = true,
+										autoImportCompletions = true,
+										diagnosticMode = "workspace",
+										useLibraryCodeForTypes = true,
+										extraPaths = { "." },
+									},
+								},
+							},
+						})
+					end,
+
+					lua_ls = function()
+						lsps.lua_ls.setup({
+							on_attach = require("tigh-latte.lsp").make_on_attach(),
+							capabilities = capabilities,
+							filetypes = { "lua" },
+							root_dir = require("lspconfig/util").root_pattern(".git"),
+							single_file_support = true,
+							settings = {
+								Lua = {
+									runtime = {
+										version = "LuaJIT",
+										path = vim.split(package.path, ";"),
+									},
+									format = {
+										enable = true,
+										defaultConfig = {
+											table_seperator_style = "comma",
+											trailing_table_separator = "smart",
+											call_arg_parentheses = "keep",
+											quote_style = "double",
+										},
+									},
+								},
+							},
+						})
+					end,
+
+					taplo = function()
+						lsps.taplo.setup({
+							on_attach = require("tigh-latte.lsp").make_on_attach(),
+							capabilities = capabilities,
+							settings = {
+								evenBetterToml = {
+									formatter = {
+										alignEntries = true,
+										indentEntries = true,
+										indentTables = true,
+										indentString = "  ",
+									},
+								},
+							},
+						})
+					end,
+
+					cucumber_language_server = function()
+						lsps.cucumber_language_server.setup({
+							on_attach = require("tigh-latte.lsp").make_on_attach(),
+							capabilities = capabilities,
+							settings = {
+								cucumber = {
+									features = { "**/*.feature" },
+									glue = { "**/steps.go" },
+								},
+							},
+						})
+					end,
+
 				},
 			})
 		end,
