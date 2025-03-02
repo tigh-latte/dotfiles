58a06605cc3715e20945407a0658f1f09fdd91ff
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-03-02 22:24:30 +0000

    change airline whitespace character to work around vim bug

--- .config/nvim/lua/tigh-latte/plugins/airline.lua
+++ .config/nvim/lua/tigh-latte/plugins/airline.lua
@@ -4,6 +4,7 @@ return {
 		vim.cmd([[
     "let g:airline#extensions#tabline#enabled = 1
     "let g:airline#extensions#tabline#formatter = 'unique_tail_improved'
+    let g:airline#extensions#whitespace#symbol = '!'
     let g:airline_mode_map = {
       \ '__': '-',
       \ 'n': 'N',
