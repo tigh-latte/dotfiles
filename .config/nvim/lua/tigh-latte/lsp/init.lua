b2d2ddfe1937557399e49bb84ddef235bf5eb813
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-03-27 22:23:47 +0000

    remove deprecated function

--- .config/nvim/lua/tigh-latte/lsp/init.lua
+++ .config/nvim/lua/tigh-latte/lsp/init.lua
@@ -35,20 +35,17 @@ function M.setup()
 
 	require("tigh-latte.lsp.cmp")
 
-	local signs = {
-		Error = "•",
-		Warn = "•",
-		Hint = "•",
-		Info = "•",
-	}
-	for type, icon in pairs(signs) do
-		local hl = "DiagnosticSign" .. type
-		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-	end
-
 	vim.diagnostic.config({
 		virtual_text = {
 			prefix = "•",
+		},
+		signs = {
+			text = {
+				[vim.diagnostic.severity.ERROR] = "•",
+				[vim.diagnostic.severity.WARN] = "•",
+				[vim.diagnostic.severity.HINT] = "•",
+				[vim.diagnostic.severity.INFO] = "•",
+			},
 		},
 	})
 end
