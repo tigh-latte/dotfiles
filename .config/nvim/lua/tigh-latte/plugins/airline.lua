2ee683a1f8fe718af5a40c28150c7b4de6606238
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-03-28 22:48:04 +0000

    remove airline workout for old neovim bug

--- .config/nvim/lua/tigh-latte/plugins/airline.lua
+++ .config/nvim/lua/tigh-latte/plugins/airline.lua
@@ -4,7 +4,6 @@ return {
 		vim.cmd([[
     "let g:airline#extensions#tabline#enabled = 1
     "let g:airline#extensions#tabline#formatter = 'unique_tail_improved'
-    let g:airline#extensions#whitespace#symbol = '!'
     let g:airline_mode_map = {
       \ '__': '-',
       \ 'n': 'N',
