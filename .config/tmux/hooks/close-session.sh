6a14d0ce085f50e65fa07e2c4a1689655b67268c
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-09-26 21:41:53 +0100

    auto close music player when it is last session

---
+++ .config/tmux/hooks/close-session.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+__main() {
+	local num_sessions=$(tmux list-sessions | wc -l)
+
+	if [ "$num_sessions" -eq 1 ]; then
+		local session=$(tmux display-message -p '#S')
+		if [ "$session" = "_popup_music_player" ]; then
+			tmux kill-session -t "$session"
+		fi
+	fi
+}
+
+__main
