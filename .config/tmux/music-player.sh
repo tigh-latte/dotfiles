bd70d9cfc1ce88e3a7c0155710a10008914b65ea
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2025-09-26 21:28:55 +0100

    toggle is now handled by the script instead of tmux config

--- .config/tmux/music-player.sh
+++ .config/tmux/music-player.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
-session="_popup_music_player"
 
-if ! tmux has -t "$session" 2> /dev/null; then
-	session_id=$(tmux new-session -dP -s "$session" -F '#{session_id}')
-	tmux set-option -s -t "$session_id" key-table popup
-	tmux set-option -s -t "$session_id" status off
-	tmux set-option -s -t "$session_id" prefix None
-	tmux send-keys -t "$session" 'hifi-rs open' C-m
-fi
+__main() {
+	local session="_popup_music_player"
 
-tmux attach -t "$session" > /dev/null
+	local current_session=$(tmux display-message -p '#S')
+
+	if [ "$current_session" = "$session" ]; then
+		tmux detach
+		return
+	fi
+
+	if ! tmux has -t "$session" 2> /dev/null; then
+		session_id=$(tmux new-session -dP -s "$session" -F '#{session_id}')
+		tmux set-option -s -t "$session_id" key-table popup
+		tmux set-option -s -t "$session_id" status off
+		tmux send-keys -t "$session" 'hifi-rs open' C-m
+	fi
+
+	tmux attach -t "$session" > /dev/null
+}
+
+__main
