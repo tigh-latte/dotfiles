b5d6e9e1909aa392193f08e7a7992d62650eee3f
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-01-02 22:19:25 +0000

    open quickfix list on vim enter, if one is provided

--- .config/nvim/after/plugin/quickfix.lua
+++ .config/nvim/after/plugin/quickfix.lua
@@ -11,3 +11,17 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
 		end
 	end,
 })
+
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = tigh_latte_group,
+	callback = function()
+		if #vim.fn.getqflist() > 0 then
+			vim.cmd.copen()
+			vim.cmd.cfirst()
+			vim.cmd("norm! zz")
+		end
+	end,
+})
+
+vim.keymap.set("n", "]]", ":cn<CR>")
+vim.keymap.set("n", "[[", ":cp<CR>")
