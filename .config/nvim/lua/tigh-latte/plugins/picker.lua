4f0c3f23ce44a4c8c099d5e3b40f7855f3d3736a
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-01-09 16:15:33 +0000

    better picker winopts setup

--- .config/nvim/lua/tigh-latte/plugins/picker.lua
+++ .config/nvim/lua/tigh-latte/plugins/picker.lua
@@ -149,84 +149,68 @@ return { {
 
 		vim.keymap.set("n", "<Leader>gr", fzf.live_grep, { silent = true })
 		vim.keymap.set("n", "<Leader>he", fzf.help_tags, { silent = true })
-		vim.keymap.set("n", "<Leader>cre", function() fzf.lsp_references({
-			silent = true,
-			profile = "ivy",
-			winopts = {
-				height = 0.4,
-				tilte_pos = "left",
-				border = { "", "─", "", "", "", "", "", "" },
-				preview = {
-					border = function(_, m)
-						if m.type == "fzf" then
-							return "single"
-						else
-							assert(m.type == "nvim" and m.name == "prev" and type(m.layout) == "string")
-							local b = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
-							if m.layout == "down" then
-								b[1] = "├" --top right
-								b[3] = "┤" -- top left
-							elseif m.layout == "up" then
-								b[7] = "├" -- bottom left
-								b[6] = "" -- remove bottom
-								b[5] = "┤" -- bottom right
-							elseif m.layout == "left" then
-								b[3] = "┬" -- top right
-								b[5] = "┴" -- bottom right
-								b[6] = "" -- remove bottom
-							else -- right
-								b[1] = "┬" -- top left
-								b[7] = "┴" -- bottom left
-								b[6] = "" -- remove bottom
-							end
-							return b
+		local winopts = {
+			height = 0.4,
+			title_pos = "left",
+			border = { "", "─", "", "", "", "", "", "" },
+			preview = {
+				border = function(_, m)
+					if m.type == "fzf" then
+						return "single"
+					else
+						assert(m.type == "nvim" and m.name == "prev" and type(m.layout) == "string")
+						local b = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
+						if m.layout == "down" then
+							b[1] = "├" --top right
+							b[3] = "┤" -- top left
+						elseif m.layout == "up" then
+							b[7] = "├" -- bottom left
+							b[6] = "" -- remove bottom
+							b[5] = "┤" -- bottom right
+						elseif m.layout == "left" then
+							b[3] = "┬" -- top right
+							b[5] = "┴" -- bottom right
+							b[6] = "" -- remove bottom
+						else -- right
+							b[1] = "┬" -- top left
+							b[7] = "┴" -- bottom left
+							b[6] = "" -- remove bottom
 						end
-					end,
-					layout = "horizontal",
-					horizontal = "right:50%",
-				},
+						return b
+					end
+				end,
+				layout = "horizontal",
+				horizontal = "right:50%",
 			},
-		}) end, { silent = true })
+		}
+		vim.keymap.set("n", "<Leader>cim", function()
+			fzf.lsp_implementations({
+				silent = true,
+				profile = "ivy",
+				winopts = winopts,
+			})
+		end, { silent = true })
+		vim.keymap.set("n", "<Leader>cre", function()
+			fzf.lsp_references({
+				silent = true,
+				profile = "ivy",
+				winopts = winopts,
+			})
+		end, { silent = true })
+
+		local ttwinopts = vim.tbl_deep_extend("force", winopts, { preview = { horizontal = "right:70%" } })
+		vim.keymap.set("n", "<Leader>tt", function()
+			fzf.lsp_workspace_diagnostics({ silent = true, profile = "ivy", winopts = ttwinopts })
+		end)
 		vim.keymap.set("n", "<Leader>/", fzf.lsp_workspace_symbols, { silent = true })
 		vim.keymap.set("n", "<Leader>cb", fzf.git_branches, { silent = true })
+
+		local cawinopts = vim.tbl_deep_extend("force", winopts, { height = 0.2 })
 		vim.keymap.set("n", "<Leader>ca", function()
 			fzf.lsp_code_actions({
 				silent = true,
 				profile = "ivy",
-				winopts = {
-					height = 0.2,
-					tilte_pos = "left",
-					border = { "", "─", "", "", "", "", "", "" },
-					preview = {
-						border = function(_, m)
-							if m.type == "fzf" then
-								return "single"
-							else
-								assert(m.type == "nvim" and m.name == "prev" and type(m.layout) == "string")
-								local b = { "┌", "─", "┐", "│", "┘", "─", "└", "│" }
-								if m.layout == "down" then
-									b[1] = "├" --top right
-									b[3] = "┤" -- top left
-								elseif m.layout == "up" then
-									b[7] = "├" -- bottom left
-									b[6] = "" -- remove bottom
-									b[5] = "┤" -- bottom right
-								elseif m.layout == "left" then
-									b[3] = "┬" -- top right
-									b[5] = "┴" -- bottom right
-									b[6] = "" -- remove bottom
-								else -- right
-									b[1] = "┬" -- top left
-									b[7] = "┴" -- bottom left
-									b[6] = "" -- remove bottom
-								end
-								return b
-							end
-						end,
-						layout = "horizontal",
-						horizontal = "right:50%",
-					},
-				},
+				winopts = cawinopts,
 			})
 		end, { silent = true })
 	end,
