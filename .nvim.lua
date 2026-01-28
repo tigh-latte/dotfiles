0618b59219ae06eeff1d0d46e6450ced4fe8adbf
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-01-28 02:39:24 +0000

    have reload command for swayosd-server use somewm client instead of swaymsg

--- .nvim.lua
+++ .nvim.lua
@@ -2,30 +2,9 @@ local augroup = vim.api.nvim_create_augroup("tigh-latte-dotfiles", { clear = fal
 
 vim.api.nvim_create_autocmd("BufWritePost", {
 	group = augroup,
-	pattern = ".config/sway/*",
-	command = "!swaymsg reload",
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = augroup,
-	pattern = ".config/swaync/*",
-	command = "!swaync-client -R",
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = augroup,
-	pattern = ".config/waybar/*",
-	callback = function()
-		vim.cmd("!swaymsg exec pkill waybar")
-		vim.cmd("!swaymsg exec waybar")
-	end,
-})
-
-vim.api.nvim_create_autocmd("BufWritePost", {
-	group = augroup,
 	pattern = ".config/swayosd/*",
 	callback = function()
-		vim.cmd("!swaymsg exec pkill swayosd-server")
-		vim.cmd("!swaymsg exec swayosd-server")
+		vim.cmd("!pkill swayosd-server")
+		vim.cmd([[!somewm-client eval 'require("awful").spawn("swayosd-server")']])
 	end,
 })
