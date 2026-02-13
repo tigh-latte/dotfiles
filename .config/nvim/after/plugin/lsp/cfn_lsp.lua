67778398478815db0e1e5896d11aa8023ea1eb50
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-02-13 21:09:22 +0000

    formatting

--- .config/nvim/after/plugin/lsp/cfn_lsp.lua
+++ .config/nvim/after/plugin/lsp/cfn_lsp.lua
@@ -1,5 +1,8 @@
 vim.lsp.config('cfn_lsp', {
-	cmd = { "node", "/home/tigh/Dev/cfnlsp/cfn-lsp-server-standalone.js", "--stdio" },
+	cmd = {
+		"node",
+		"/home/tigh/Dev/github.com/aws-cloudformation/cloudformation-languageserver/bundle/production/cfn-lsp-server-standalone.js",
+		"--stdio" },
 	filetypes = { "yaml.cf" },
 	init_options = {
 		aws = {
