e224cbf8b6f5fd80de635a99eed109f0c9ab3236
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-03-16 21:50:04 -0300

    auto install treesitter parser for filetype if not installed

--- .config/nvim/lua/tigh-latte/plugins/tree-sitter.lua
+++ .config/nvim/lua/tigh-latte/plugins/tree-sitter.lua
@@ -9,6 +9,33 @@ require("nvim-treesitter").install({
 
 vim.treesitter.language.register("bash", "zsh")
 
+-- auto init treesitter parsers if they exist.
+-- otherwise, try to install.
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function(args)
+		local lang = vim.treesitter.language.get_lang(vim.bo.ft)
+		local parser = vim.treesitter.get_parser(args.buf, lang, { error = false })
+		if not parser then
+			-- attempt to install
+			local parsers = require("nvim-treesitter.parsers")
+			if parsers[lang] ~= nil then
+				require("nvim-treesitter").install({ lang })
+				local t = assert(vim.uv.new_timer())
+				t:start(1000, 1000, vim.schedule_wrap(function()
+					local p = vim.treesitter.get_parser(args.buf, lang, { error = false })
+					if p then
+						vim.treesitter.highlighter.new(p)
+						t:stop()
+					end
+				end))
+			end
+		else
+			vim.treesitter.highlighter.new(parser)
+		end
+	end
+})
+
 vim.api.nvim_create_autocmd("BufReadPre", {
 	once = true,
 	callback = function()
