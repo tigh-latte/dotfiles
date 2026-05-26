be90d5bea1c60c3b74936c6602f2cf48e882c02c
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-05-26 14:26:32 +0100

    fix(highlights): highlight golang correct for bracketed type casts

--- .config/nvim/queries/go/highlights.scm
+++ .config/nvim/queries/go/highlights.scm
@@ -17,6 +17,19 @@
     "int32" "int64" "int8" "rune" "string" "uint" "uint16" "uint32" "uint64" "uint8" "uintptr"
   ))
 
+; (*string)(someval)
+; (string)(someval)
+(call_expression
+  function: (parenthesized_expression
+    [
+      (unary_expression
+        operand: (identifier) @type.builtin)
+      (identifier) @type.builtin
+    ]
+    (#any-of? @type.builtin
+      "any" "bool" "byte" "complex128" "complex64" "error" "float32" "float64" "int" "int16"
+      "int32" "int64" "int8" "rune" "string" "uint" "uint16" "uint32" "uint64" "uint8" "uintptr")))
+
 ((identifier) @function.builtin
   (#any-of? @function.builtin
     "append" "cap" "clear" "close" "complex" "copy" "delete" "imag" "len" "make" "max" "min" "new"
