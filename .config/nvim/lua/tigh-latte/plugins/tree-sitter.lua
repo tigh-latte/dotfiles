45d3de776561e91c008aacaa7f551c415131ed3b
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-06-02 21:28:56 +0100

    slight rearrange to reduce indenting of tree-sitter impl

--- .config/nvim/lua/tigh-latte/plugins/tree-sitter.lua
+++ .config/nvim/lua/tigh-latte/plugins/tree-sitter.lua
@@ -17,22 +17,22 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function(args)
 		local lang = vim.treesitter.language.get_lang(vim.bo.ft)
 		local parser = vim.treesitter.get_parser(args.buf, lang, { error = false })
-		if not parser then
-			-- attempt to install
-			local parsers = require("nvim-treesitter.parsers")
-			if parsers[lang] ~= nil then
-				require("nvim-treesitter").install({ lang })
-				local t = assert(vim.uv.new_timer())
-				t:start(1000, 1000, vim.schedule_wrap(function()
-					local p = vim.treesitter.get_parser(args.buf, lang, { error = false })
-					if p then
-						vim.treesitter.highlighter.new(p)
-						t:stop()
-					end
-				end))
-			end
-		else
+		if parser then
 			vim.treesitter.highlighter.new(parser)
+			return
+		end
+
+		local parsers = require("nvim-treesitter.parsers")
+		if parsers[lang] ~= nil then
+			require("nvim-treesitter").install { lang }
+			local t = assert(vim.uv.new_timer())
+			t:start(1000, 1000, vim.schedule_wrap(function()
+				local p = vim.treesitter.get_parser(args.buf, lang, { error = false })
+				if p then
+					vim.treesitter.highlighter.new(p)
+					t:stop()
+				end
+			end))
 		end
 	end
 })
