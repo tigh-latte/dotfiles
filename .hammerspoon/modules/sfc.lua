4c625795402f956c027c582fd8de52705882713b
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-07-03 10:54:08 +0100

    sfc fixes around screensharing + window toggling

--- .hammerspoon/modules/sfc.lua
+++ .hammerspoon/modules/sfc.lua
@@ -1,7 +1,13 @@
-local M = {
+M = {
 	windows = {},
 	listeners = {}
 }
+
+function M.dump()
+	for app, wins in pairs(M.windows) do
+		print(app, "{", table.concat(wins, " "), "}")
+	end
+end
 
 function M.sfc(app)
 	if not M.windows[app] or #M.windows[app] <= 1 then
@@ -10,12 +16,11 @@ function M.sfc(app)
 	end
 
 	local current_window = hs.window.focusedWindow()
-	local current_app = current_window and current_window:application()
 
-	local wf = hs.window.filter.new({ app })
+	local wf = hs.window.filter.new({ [app] = { allowRoles = 'AXStandardWindow' } })
 	local windows = wf:getWindows(hs.window.filter.sortByFocusLast)
 	if not windows or #windows ~= #M.windows[app] then
-		hs.alert.show("sfc out of sync")
+		-- hs.alert.show("sfc out of sync")
 	end
 	local last_focused = windows[1]
 	if last_focused:id() ~= current_window:id() then
@@ -31,23 +36,36 @@ function M.sfc(app)
 	end
 end
 
+function M._track(win, app)
+	if not win:isStandard() then return end
+	if win:title() == "Slack" and app == "Slack" then -- huddle window
+		local frame = win:frame()
+		if frame.h < frame.w / 2 then              -- more huddle window checks
+			return
+		end
+	end
+	if not M.windows[app] then M.windows[app] = {} end
+	table.insert(M.windows[app], win:id())
+end
+
+function M._untrack(win, app)
+	if not M.windows[app] then return end
+	local wid = win:id()
+	for i, id in ipairs(M.windows[app]) do
+		if id == wid then
+			table.remove(M.windows[app], i)
+			break
+		end
+	end
+end
+
 M.listeners = {
-	on_create = hs.window.filter.default:subscribe(hs.window.filter.windowCreated, function(win, app, _)
-		if not M.windows[app] then
-			M.windows[app] = {}
-		end
-		table.insert(M.windows[app], win:id())
-	end, true),
-	on_destroy = hs.window.filter.default:subscribe(hs.window.filter.windowDestroyed, function(win, app, _)
-		if not M.windows[app] then return end
-		local wid = win:id()
-		for i, id in ipairs(M.windows[app]) do
-			if id == wid then
-				table.remove(M.windows[app], i)
-				break
-			end
-		end
-	end),
+	on_create = hs.window.filter.default:subscribe(hs.window.filter.windowCreated, M._track, true),
+	on_destroy = hs.window.filter.default:subscribe(hs.window.filter.windowDestroyed, M._untrack),
+	on_hidden = hs.window.filter.default:subscribe(hs.window.filter.windowHidden, M._untrack, true),
+	on_unhidden = hs.window.filter.default:subscribe(hs.window.filter.windowUnhidden, M._track),
+	on_minimise = hs.window.filter.default:subscribe(hs.window.filter.windowMinimized, M._untrack, true),
+	on_unminimise = hs.window.filter.new():setDefaultFilter({}):subscribe(hs.window.filter.windowUnminimized, M._track),
 }
 
 return M
