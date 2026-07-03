ae6c0d0d2d42ed88961223bc0ac0879c8274591e
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-07-03 10:57:42 +0100

    set font based on dpi for apple to handle text size between laptop/studio monitor screens + home monitor

--- .config/wezterm/apple/font.lua
+++ .config/wezterm/apple/font.lua
@@ -1,5 +1,15 @@
 ---@module "wezterm"
 
+wezterm.on('window-config-reloaded', function(window, pane)
+	local dimensions = window:get_dimensions()
+	if dimensions.dpi == 144 then
+		local overrides = window:get_config_overrides()
+		overrides.font_size = 11.0
+		window:set_config_overrides(overrides)
+	end
+end)
+
+
 return {
 	---@param config Config
 	setup = function(config)
