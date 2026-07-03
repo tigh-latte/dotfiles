4a259217e015a22c626bf9f5ca3ff66c598f25a6
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-07-03 10:53:31 +0100

    use the nowplaying-cli for audio pausing as the play button toggles

--- .hammerspoon/modules/audio.lua
+++ .hammerspoon/modules/audio.lua
@@ -2,7 +2,7 @@ hs.audiodevice.watcher.setCallback(function(event)
 	if event ~= "dOut" then return end
 	local output = hs.audiodevice.defaultOutputDevice()
 	if output:name() == "MacBook Pro Speakers" then
-		hs.eventtap.event.newSystemKeyEvent("PLAY", true):post()
+		hs.task.new("/opt/homebrew/bin/nowplaying-cli", nil, { "pause" }):start()
 		if not output:muted() then output:setMuted(true) end
 	end
 end)
