756fa0663bf61ba269fe866be5373134b785b01f
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-07-10 23:00:13 +0100

    goimpl improvements, now comes with a UI

--- .config/nvim/lua/tigh-latte/langs/go.lua
+++ .config/nvim/lua/tigh-latte/langs/go.lua
@@ -1,3 +1,5 @@
+local methods = vim.lsp.protocol.Methods
+
 return function()
 	---@param err lsp.ResponseError?
 	local cb = function(err)
@@ -227,43 +229,126 @@ return function()
 		vim.cmd.e(alt_name())
 	end, { nargs = 0 })
 
-	vim.api.nvim_create_user_command("GoImpl", function(args)
+	vim.api.nvim_buf_create_user_command(0, "GoImpl", function()
 		local cur_name = vim.treesitter.get_node()
-		while cur_name and cur_name:type() ~= "type_spec" do
+		while cur_name and cur_name:type() ~= "type_declaration" do
 			cur_name = cur_name:parent()
 		end
 		if not cur_name then
 			vim.notify("no type under cursor", vim.log.levels.ERROR)
 			return
 		end
-		local line, char, eline, echar = cur_name:field("name")[1]:range()
+		cur_name = cur_name:child(1)
+		local line, char, eline, echar = cur_name:range()
 
-		local _do = function(iface)
-			local type_name = vim.api.nvim_buf_get_text(0, line, char, eline, echar, {})[1]
-			vim.system({ "impl", type_name, iface, }, {}, function(out)
-				vim.schedule(function()
-					if out.code ~= 0 then
-						vim.notify(out.stderr)
+		local _do = function(iface_name)
+			get_client():request(methods.workspace_executeCommand, {
+				command = "gopls.implement_interface",
+				arguments = { {
+					location = {
+						uri = vim.uri_from_bufnr(0),
+						range = {
+							start = { line = line, character = char },
+							["end"] = { line = eline, character = echar },
+						},
+					},
+					interface = iface_name, -- defined in gopls but not needed. sending anyway.
+				} },
+				formFields = { { ["type"] = "string" } },
+				formAnswers = { iface_name },
+			}, cb_restart)
+		end
+
+		local previewer = require("fzf-lua.previewer.builtin").buffer_or_file:extend()
+		function previewer:new(o, _opts, fzf_win)
+			previewer.super.new(self, o, _opts, fzf_win)
+			setmetatable(self, previewer)
+			return self
+		end
+
+		function previewer:parse_entry(entry_str)
+			if not self.results then return end
+			local entry = self.results[entry_str]
+			if not entry or entry == "" then return end
+			return {
+				path = vim.uri_to_fname(entry.location.uri),
+				line = entry.location.range.start.line + 1,
+				col = entry.location.range.start.character + 1,
+			}
+		end
+
+		local last_request_id
+		---@return lsp.Handler
+		local function build_lsp_handler(handle)
+			return function(err, results)
+				(function()
+					last_request_id = nil
+					if err ~= nil then
+						vim.notify(err.message, vim.log.levels.ERROR)
+						return
+					end
+					if results == nil then
 						return
 					end
 
-					local lines = vim.split(vim.trim(out.stdout), "\n", {})
-					if #lines <= 1 then return end
+					previewer.results = {}
 
-					table.insert(lines, "")
-					vim.fn.append(line, lines)
-				end)
-			end)
+					for _, result in ipairs(results) do
+						if vim.lsp.protocol.SymbolKind[result.kind] ~= "Interface" then
+							goto continue
+						end
+
+						local key = (function()
+							if not result.containerName:find "/" then
+								return result.name
+							end
+							-- here, the result.containerName is the package path (github.com/blahblah/blah)
+							-- and the result.name is the interface name. However, the inferface name can
+							-- _sometimes_ contain the package name (blah.Blaher).
+							-- In this case, a direct combine would create
+							-- github.com/blahblah/blah.blah.Blaher
+							-- To prevent this, we check if result.name has a ".", and if yes, cut everyting
+							-- before it
+							local name = result.name
+							local accessor_idx = result.name:find "%."
+							name = accessor_idx and name:sub(accessor_idx + 1) or name
+
+							return result.containerName .. "." .. name
+						end)()
+
+						previewer.results[key] = result
+						handle(key)
+
+						::continue::
+					end
+				end)()
+			end
 		end
-		if args.args == "" then
-			vim.ui.input({ prompt = "iface > " }, function(input)
-				if not input then return end
-				_do(input)
-			end)
-		else
-			_do(args.args)
+
+		local function generate_contents(query)
+			if last_request_id then get_client():cancel_request(last_request_id) end
+			return function(handle)
+				query = (query and query[1]) or ""
+				_, last_request_id = get_client():request(
+					methods.workspace_symbol,
+					{ query = query },
+					build_lsp_handler(handle)
+				)
+			end
 		end
-	end, { nargs = '*' })
+
+		require("fzf-lua").fzf_live(generate_contents, {
+			prompt = "Find an interface > ",
+			actions = {
+				default = function(items)
+					if not items or #items == 0 then return end
+					local item = items[1]
+					_do(item)
+				end
+			},
+			previewer = previewer,
+		})
+	end, { nargs = 0 })
 
 	local opts = { remap = false, buffer = true, silent = true }
 	vim.keymap.set("n", "<Leader>gav", vim.cmd.GoAltV, opts)
