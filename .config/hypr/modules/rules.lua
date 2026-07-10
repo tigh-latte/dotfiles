655116565618f1b267b3f77794016866b4d542a5
Tighearnán Carroll <tighearnan.carroll@gmail.com>
2026-07-10 22:34:17 +0100

    Load applications launched by steam in their own workspace
    There is no way to reliable tell from window decoration information if
    an launched window is for a game. There is no consistency between the
    classes or the titles.
    
    So, the only thing that I can do currently is check to see if an
    application was launched by steam (and maybe other launchers in the
    future) by traversing /proc/:pid. If it is found to have been launched
    by steam, then we give it its own workspace.

--- .config/hypr/modules/rules.lua
+++ .config/hypr/modules/rules.lua
@@ -58,3 +58,34 @@ hl.workspace_rule {
 	no_rounding = true,
 }
 
+-- the workspace of a rule doesn't appear to applied after a window is opened, meaning
+-- listening for a tag on the window to move a window to a workspace won't work.
+-- Instead, use a dispatch to set the workspace.
+---@param win HL.Window
+hl.on("window.open", function(win)
+	local function launched_by_steam(pid, depth)
+		if not depth then depth = 1 end
+		if depth == 10 then return false end
+		if not pid or pid <= 0 then return false end
+
+		local pfile = "/proc/" .. tostring(pid) .. "/stat"
+		local ok, f = pcall(io.open, pfile, "r")
+		if not ok then return false end
+		if not f then return false end
+		local contents = f:read("*a") --[[@as string]]
+		f:close()
+		if not contents or contents == "" then return false end
+
+		if contents:find("^" .. pid .. " %(steam%)") or contents:find("^" .. pid .. " %(steamwebhelper%)") then
+			return depth > 1
+		end
+		local parent = contents:match "%) %s*%S+%s*(%d+)"
+		if not parent then return false end
+		return launched_by_steam(tonumber(parent), depth + 1)
+	end
+
+	if launched_by_steam(win.pid) then
+		hl.dispatch(hl.dsp.window.tag({ tag = "steam_game", window = win }))
+		hl.dispatch(hl.dsp.window.move({ workspace = "4", follow = true, window = win }))
+	end
+end)
